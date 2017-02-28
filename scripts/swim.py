c43614c59bf630230fbbf03f9bda15ebf0844a38
cPolaris <ctg868@gmail.com>
2017-02-27 21:53:46 -0600

    add some more

---
+++ scripts/swim.py
@@ -0,0 +1,50 @@
+# I have a file tree...
+# .
+# ├── d1
+# │   ├── d11
+# │   │   └── foo11
+# │   └── foo1
+# ├── d2
+# ├── d3
+# │   └── d33
+# │       └── d333
+# │           └── foo333
+# ├── foo2
+# └── foo3
+# I'd like all the files to be moved to the root directory.
+# So that it looks like:
+# .
+# ├── d1
+# │   ├── d11
+# │   │   └── foo11
+# │   └── foo1
+# ├── d2
+# ├── d3
+# │   └── d33
+# │       └── d333
+# │           └── foo333
+# ├── foo2
+# └── foo3
+# I call this operation 'swim the files' because they are all
+# raised to the 'surface' level of the tree
+import os
+
+BASE = os.path.abspath('.')
+
+def do_dir(curr_path, prefix):
+    print(prefix + 'Doing ' + os.path.relpath(curr_path, BASE))
+    for f in os.listdir(curr_path):
+        fp = os.path.join(curr_path, f)
+        if os.path.isdir(fp):
+            do_dir(fp, prefix + '\t')
+        elif os.path.isfile(fp):
+            top = os.path.join(BASE, f)
+            print(prefix + 'Moving ' + fp + ' -> ' + top)
+            os.rename(fp, top)
+    print(prefix + 'Removing directory ' + os.path.relpath(curr_path, BASE))
+    os.rmdir(curr_path)
+
+if __name__ == '__main__':
+    for f in os.listdir():
+        if os.path.isdir(f):
+            do_dir(os.path.abspath(f), '')
